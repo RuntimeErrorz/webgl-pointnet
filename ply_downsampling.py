c76533a80ae9f3bb4b2a428581716a68eda9aa98
RuntimeError <1059171929@qq.com>
2022-09-16 17:58:32 +0800

    Add filter function

--- ply_downsampling.py
+++ ply_downsampling.py
@@ -2,4 +2,4 @@ import open3d as o3d
 
 pcd = o3d.io.read_point_cloud('../example/birmingham_block_2.ply')
 downpcd = pcd.voxel_down_sample(voxel_size=0.05)
-o3d.io.write_point_cloud("downsample.pcd", pcd)
+o3d.io.write_point_cloud("downsample.ply", pcd)
